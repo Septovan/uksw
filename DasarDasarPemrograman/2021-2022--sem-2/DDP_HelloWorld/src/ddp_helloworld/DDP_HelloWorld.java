710e078043b6957057b6da6244174368370fa8ed
Septovan <672015027@student.uksw.edu>
2022-02-17 14:37:37 +0700

    Added HelloWorld Java

---
+++ DasarDasarPemrograman/2021-2022--sem-2/DDP_HelloWorld/src/ddp_helloworld/DDP_HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ddp_helloworld;
+
+/**
+ *
+ * @author Septovan D. S. Saian
+ */
+public class DDP_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("****"); 
+        System.out.println("****");
+        System.out.println("****");
+        System.out.println("****");               
+    }
+    
+}
